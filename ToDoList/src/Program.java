6809b4972d1a6a066b83898c1988ddc18bf39bc9
Majikujanisch <karottennase123@gmail.com>
2020-03-15 18:17:19 +0100

    create task and list tasks

--- ToDoList/src/Program.java
+++ ToDoList/src/Program.java
@@ -11,6 +11,8 @@ public class Program{
     private static Scanner reader = new Scanner(System.in);
     public static void main(String[] args) {
         char choice, choicecreate;
+        String header, text;
+        int year, month, day, hour, minute;
         String file = "Tasks/filetask.bin";
         List<Task> tasks = new ArrayList<Task>();
         List<Task> TasksfromFile;
@@ -28,8 +30,29 @@ public class Program{
                     choicecreate = createMenu();
                     switch(choicecreate){
                         case 'm':
+                            System.out.println("Fach:");
+                            header = reader.next();
+                            System.out.println("Aufgabe:");
+                            text = reader.next();
+                            System.out.println("Bis[Jahr|Monat|Tag]:");
+                            year = reader.nextInt();
+                            month = reader.nextInt();
+                            day = reader.nextInt();
+                            System.out.println("Bis[Stunde(bis 23)|minute]:");
+                            hour = reader.nextInt();
+                            minute = reader.nextInt();
+                            tasks.add(new Task(header,text,year,month,day,hour,minute));
                             break;
                         case 'o':
+                            System.out.println("Fach:");
+                            header = reader.next();
+                            System.out.println("Aufgabe:");
+                            text = reader.next();
+                            System.out.println("Bis[Jahr|Monat|Tag]:");
+                            year = reader.nextInt();
+                            month = reader.nextInt();
+                            day = reader.nextInt();
+                            tasks.add(new Task(header,text,year,month,day));
                             break;
                         default:
                             System.out.println("Falscher Buchstabe");
@@ -37,6 +60,15 @@ public class Program{
                     }
                     break;
                 case 'l':
+                    choicecreate = listMenu();
+                    switch(choicecreate){
+                        case 'a':
+                            break;
+                        case 'l':
+                            break;
+                    }
+
+
                     break;
                 case'b':
                     System.out.println("Programm wird beendet");
@@ -54,7 +86,7 @@ public class Program{
     public static char menu(){
         char choice;
         System.out.println("Aufgabe erstellen.............c");
-        System.out.println("List aller offenen Aufgaben...l");
+        System.out.println("Liste aller offenen Aufgaben...l");
         System.out.println("Beenden.......................b");
         choice = reader.next().toLowerCase().charAt(0);
         return choice;
@@ -67,6 +99,11 @@ public class Program{
         return choice;
     }
 
-
-
+    public static char listMenu(){
+        char choice;
+        System.out.println("Aufgabe bearbeiten...a");
+        System.out.println("Aufgabe löschen......l");
+        choice = reader.next().toLowerCase().charAt(0);
+        return choice;
+    }
 }
